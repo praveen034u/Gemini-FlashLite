21b190d68a7e9bad6b022b5acbd69492624d3b3b
Rohit Saundankar <saundankarro@gmail.com>
2025-06-22 11:46:30 -0400

    Attempt at keeping history during conversation. Not tested

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from gemini_langchain import GeminiLLM
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+
+
+# memory = ConversationBufferMemory(memory_key="history", return_messages=False)
+
+# prompt_template = PromptTemplate(
+#     input_variables=["history", "PromptInput"],
+#     template="""
+#     The following is a conversation between a user and an assistant.
+#     {history}
+#     User: {PromptInput}
+#     Assistant:"""
+# )
+
+# llm = GeminiLLM()
+# conversation = LLMChain(
+#     llm = llm,
+#     prompt = prompt_template,
+#     memory = memory,
+#     verbose = True
+# )
 
 app = FastAPI()
 
